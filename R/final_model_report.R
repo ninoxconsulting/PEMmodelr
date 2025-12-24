c3deec782cab4a36488f6f15e83768d1c8ef0074
Genevieve Perkins <genevieve.c.perkins@gmail.com>
2025-12-24 10:25:58 -0800

    update params for report

--- R/final_model_report.R
+++ R/final_model_report.R
@@ -4,11 +4,21 @@
 #' Inputs are handed to a RMD report/ script.
 #' Outputs include the markdown report, the cross validation object,
 #' and a binary model (RDS) that can then be used to predict on new data.
+#'
+#'
+#' @param model_name A character string to define the model. Default will use balance combination
 #' @param final_data  final model training point
+#' @param bec A character with the BEC label to use. FOr example "ICHmc1".
+#' @param covars A vector with the names of covariates to use. These match raster names.
+#' @param extra_pts logical. If TRUE, extra points will be included. Default is FALSE.
+#' @param mtry numeric. This is the output based on output of hyperparamter model tuning (default = ??)
+#' @param min_n numeric. This is the output based on output of hyperparamter model tuning (default = ??)
+#' @param ntrees numeric. Number of trees to use in random forest model. Default is 151.
+#' @param downsample_ratio A vector of numeric downsampling values (10 - 100), NA if not using
+#' @param smote_ratio A vector of numeric downsampling values (0.1 - 0.9), NA if not using
+#' @param out_dir OPTIONAL: only needed if detailed_output = TRUE. location of filepath there detailed outputs to be stored
 #' @param final_model  final model object
-#' @param mbaldf the balance options applied to model
-#' @param out_bgc_dir  output directory  This defaults to the project's root directory OR where the RMD script is saved.
-#' @param extra_pts  extra points to be included in the model
+#'
 #' @export
 #' @examples
 #' \dontrun{
@@ -30,8 +40,8 @@ final_model_report <- function(model_name,
                                ){
 
   ## create destination folder
-  ifelse(!dir.exists(file.path(out_bgc_dir)),
-         dir.create(file.path(out_bgc_dir)), FALSE)
+  ifelse(!dir.exists(file.path(out_dir)),
+         dir.create(file.path(out_dir)), FALSE)
 
   #RMD <- system.file("rmd_template", "final_model_report.rmd", package = "PEMmodelr")
   RMD <- fs::path_package("PEMmodelr", "extdata/final_model_report.rmd")
